ce1a3ede080318944e5a877fcc3bbdb66904aaba
nevercodin <109537251+nevercodin@users.noreply.github.com>
2020-11-07 19:08:56 +0000

    iwlwifi: always copy first 16 bytes of commands

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -3,4 +3,5 @@
 //	Musical Sound Generator Framework
 //
 //  Created by Hasebe Masahiko on 2021/10/25.
-//  Copyright (c) 2021 Ha
+//  Copyright (c) 2021 Hasebe Masahiko.
+//  Released under the MIT

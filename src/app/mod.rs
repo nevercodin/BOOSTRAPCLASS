dd68e00ecdfd570ecfc024f11ca00c877bc68ad0
nevercodin <109537251+nevercodin@users.noreply.github.com>
2020-11-16 17:43:26 +0000

    Remove extraneous space

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -16,4 +16,4 @@ pub mod sgf;
 use crate::app::va::*;
 use crate::app::sg::*;
 use crate::app::sgf::*;
-pub fn get_inst(inst_number:usize, vol:u8,
+pub fn get_inst(inst_number:usize, vol:u8, pan:u8, exp:u8) -> Box<dyn msgf_

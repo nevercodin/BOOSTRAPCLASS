5910b1b75002d649d29777c4d391b08b373b6115
nevercodin <109537251+nevercodin@users.noreply.github.com>
2020-11-22 07:13:47 +0000

    Bumped version to 0.2.1

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -17,4 +17,5 @@ use crate::app::va::*;
 use crate::app::sg::*;
 use crate::app::sgf::*;
 pub fn get_inst(inst_number:usize, vol:u8, pan:u8, exp:u8) -> Box<dyn msgf_inst::Inst> {
-    if inst_numb
+    if inst_number < 8 {
+        println!("Changed Va: {

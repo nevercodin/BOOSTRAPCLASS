5817da588fa445a81454fabc5ff8e8b191849b7c
nevercodin <109537251+nevercodin@users.noreply.github.com>
2020-12-07 06:37:08 +0000

    Delete build-impl.xml

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -29,4 +29,4 @@ pub fn get_inst(inst_number:usize, vol:u8, pan:u8, exp:u8) -> Box<dyn msgf_inst:
         println!("Changed Sgf {}",inst_number);
         Box::new(sgf_inst::InstSgf::new(0,vol,pan,exp))
     }
-    else 
+    else if inst_number == 10 { //  for Touch Key

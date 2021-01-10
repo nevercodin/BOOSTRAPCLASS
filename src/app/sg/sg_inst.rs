e8b64878d4b36f929422bd6eff76d24dd15dd368
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-01-10 11:02:27 +0000

    Add podspec.

--- src/app/sg/sg_inst.rs
+++ src/app/sg/sg_inst.rs
@@ -34,3 +34,51 @@ pub struct InstSg {
     vce: Option<Box<sg_voice::VoiceSg>>,    // 発音中の Voice
     active_vce_index: i8,                  // 発音中の vcevec のIndex
     inst_number: usize,
+    mdlt: f32,  //  0.0..0.5
+    pit: f32,   //  [cent]
+    vol: u8,    //  0..127
+    pan: f32,   //  -1..0..+1
+    exp: u8,    //  0..127
+    spmsg: [u8;4],    //  special message for SG
+    inst_prm: Rc<Cell<sg_prm::SynthParameter>>,
+}
+const NO_NOTE:i8 = -1;
+//---------------------------------------------------------
+//		Implements
+//---------------------------------------------------------
+impl MsgfDisplay for InstSg {}
+impl NoteSg {
+    fn new(note: u8, _vel: u8) -> Self {
+        Self {note, _vel, off: false,}
+    }
+}
+impl Drop for InstSg {
+    fn drop(&mut self) {self.vcevec.clear();}
+}
+//---------------------------------------------------------
+impl msgf_inst::Inst for InstSg {
+
+    fn change_inst(&mut self, mut inst_number: usize, vol: u8, pan: u8, exp: u8) {
+        let max_tone = sg_prm::SG_MAX_TONE_COUNT;
+        if inst_number >= max_tone {
+            inst_number = max_tone-1;
+        }
+        let _ = &self.inst_prm.replace(sg_prm::SG_TONE_PRM[inst_number]);
+        self.inst_number = inst_number;
+        //self.mdlt = self.inst_prm.get().osc.lfo_depth;
+        self.pit = 0.0;
+        self.vol = vol;
+        self.pan = Self::calc_pan(pan);
+        self.exp = exp;
+    }
+    fn note_off(&mut self, dt2: u8, _dt3: u8) {
+        let nt_idx = self.search_note(dt2);
+        if nt_idx == NO_NOTE {return}
+        if nt_idx == self.active_vce_index {
+            // sounding voice
+            if let Some(cur_vce) = &mut self.vce {
+                cur_vce.note_off();
+            }
+            self.vcevec[nt_idx as usize].off = true;
+        }
+        else {

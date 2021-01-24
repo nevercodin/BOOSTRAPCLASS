adb280953e8e516c5d536685e1d761210d0ec93d
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-01-24 13:38:45 +0000

    Changes and version

--- src/app/sg/sg_prm.rs
+++ src/app/sg/sg_prm.rs
@@ -22,4 +22,5 @@ pub struct SynthParameter {
 pub const SG_MAX_TONE_COUNT:usize = 1;
 pub const SG_TONE_PRM: [SynthParameter; SG_MAX_TONE_COUNT] = [
     SynthParameter {
-    
+        osc: AdditiveParameter {
+           

4e4c38d367f4d49606d4012c292ec8f73affe79d
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-03-07 13:46:46 +0000

    under construction

--- src/app/sg/sg_voice.rs
+++ src/app/sg/sg_voice.rs
@@ -28,3 +28,39 @@ pub struct VoiceSg {
     vel: u8,
     status: NoteStatus,
     damp_counter: u32,
+    lvl_check_buf: msgf_afrm::AudioFrame,
+    // Synth
+    osc: msgf_additive::Additive,
+    aeg: msgf_aeg::Aeg,
+    lfo: msgf_lfo::Lfo,
+    max_note_vol: f32,
+    ended: bool,
+    vowel_x: f32,   // -1..0..1
+    vowel_y: f32,   // -1..0..1
+}
+//---------------------------------------------------------
+//		Implements
+//---------------------------------------------------------
+impl MsgfDisplay for VoiceSg {}
+impl PartialEq for VoiceSg {
+    fn eq(&self, other: &Self) -> bool {
+        self.note == other.note && self.vel == other.vel
+    }
+}
+//---------------------------------------------------------
+impl msgf_voice::Voice for VoiceSg {
+    fn start_sound(&mut self) {
+        self.aeg.move_to_attack();
+        self.lfo.start();
+    }
+    fn slide(&mut self, note:u8, vel:u8) {
+        self.note = note;
+        self.vel = vel;
+        self.status = NoteStatus::DuringNoteOn;
+        self.damp_counter = 0;
+        self.osc.change_note(note);
+        self.aeg.move_to_attack();
+        self.lfo.start();
+    }
+    fn note_off(&mut self) {
+        self.status = NoteStatus::AfterNoteOff;

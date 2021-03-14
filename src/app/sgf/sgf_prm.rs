6fb831a9ac9eb19a14ddc991deaccbbef0e76cbf
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-03-14 05:55:36 +0000

    Publish page-17 index

--- src/app/sgf/sgf_prm.rs
+++ src/app/sgf/sgf_prm.rs
@@ -19,3 +19,27 @@ pub struct SynthParameter {
     pub aeg: AegParameter,
     pub lfo: LfoParameter,
 }
+
+pub const SGF_MAX_TONE_COUNT:usize = 1;
+pub const SGF_TONE_PRM: [SynthParameter; SGF_MAX_TONE_COUNT] = [
+    SynthParameter {
+        osc: VocalParameter {
+            coarse_tune: 0,     //  i32 : 0 means tuning of A=440[Hz]
+            fine_tune: 0.0,     //  f32 : 1.0 means 1[cent]
+            lfo_depth: 0.02,    //  f32 : 1.0 means +-1oct.
+       },
+        aeg: AegParameter {
+            attack_rate: 0.6,   //  0.0-1.0
+            decay_rate: 0.05,   //  0.0-1.0 : 1.0 means no decay and no sustain level
+            sustain_level: 0.5, //  1 means same value as Attack Level
+            release_rate: 0.02, //  0.0-1.0
+        },
+        lfo: LfoParameter {
+            freq: 6.0,          //  [Hz]
+            wave: LfoWave::Tri,
+            direction: LfoDirection::LfoBoth,
+            fadein_time: 100,   //  1: AUDIO_FRAME_PER_CONTROL / SAMPLING_FREQ (=3msec)
+            delay_time: 100,    //    same as above
+        },
+    },
+];
---
+++ src/app/sgf/sgf_voice.rs
@@ -0,0 +1,19 @@
+
+//
+//  sgf_voice.rs
+//	Musical Sound Generator Framework
+//      Sing Voice Class
+//
+//  Created by Hasebe Masahiko on 2022/06/11.
+//  Copyright (c) 2022 Hasebe Masahiko.
+//  Released under the MIT license
+//  https://opensource.org/licenses/mit-license.php
+//
+use std::rc::Rc;
+use std::cell::Cell;
+use crate::msgf_if;
+use crate::core::*;
+use crate::core::msgf_voice::*;
+use crate::core::msgf_disp::MsgfDisplay;
+use crate::engine::*;
+use crate::engine::msgf_gen::Engine;

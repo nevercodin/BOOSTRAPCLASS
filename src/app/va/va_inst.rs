dcac1cdfaa3734cda54a4467063edc6e23e9947a
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-05-18 22:54:00 +0000

    Update PHPDoc

--- src/app/va/va_inst.rs
+++ src/app/va/va_inst.rs
@@ -52,4 +52,6 @@ impl msgf_inst::Inst for InstVa {
         let max_tone = va_prm::MAX_TONE_COUNT;
         let mut inst_num = inst_number;
         if inst_number >= max_tone {
-            inst_num = max_tone-1
+            inst_num = max_tone-1;
+        }
+        let prm = Rc::new(

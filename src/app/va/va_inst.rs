61e8f10af8e9b59a1ca047bc2ef3e8a19c8a53a5
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-06-01 17:03:11 +0000

    added setup script

--- src/app/va/va_inst.rs
+++ src/app/va/va_inst.rs
@@ -76,4 +76,5 @@ impl msgf_inst::Inst for InstVa {
             inst_number = max_tone-1;
         }
         let _ = &self.inst_prm.replace(va_prm::TONE_PRM[inst_number]);
-        self.delay = msgf_delay::Delay::new(&self.i
+        self.delay = msgf_delay::Delay::new(&self.inst_prm.get().delay);
+        

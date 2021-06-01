639d85a065c52f186ea111a0f08778ba4cb65cdc
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-06-01 05:27:52 +0000

    Update css.

--- src/app/va/va_inst.rs
+++ src/app/va/va_inst.rs
@@ -71,4 +71,5 @@ impl msgf_inst::Inst for InstVa {
     }
 */
     fn change_inst(&mut self, mut inst_number: usize, vol: u8, pan: u8, exp: u8) {
-        let max_tone = va_p
+        let max_tone = va_prm::MAX_TONE_COUNT;
+        if ins

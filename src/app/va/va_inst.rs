d99e8077ca9caa7707faa36e37dc69e524370cd0
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-06-15 07:06:11 +0000

    Jeager Video

--- src/app/va/va_inst.rs
+++ src/app/va/va_inst.rs
@@ -92,4 +92,6 @@ impl msgf_inst::Inst for InstVa {
     }
     fn note_on(&mut self, dt2: u8, dt3: u8) {
         let mut new_voice = va_voice::VoiceVa::new(
-            dt2, dt3, self.mdlt, self.pit, self.vol, self.exp, Rc::clone(&sel
+            dt2, dt3, self.mdlt, self.pit, self.vol, self.exp, Rc::clone(&self.inst_prm)
+        );
+        new_voice.

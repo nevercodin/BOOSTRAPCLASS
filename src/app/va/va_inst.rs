2a5e747f8d57093466aa18ed96ec994dfd3f4684
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-07-26 15:45:18 +0000

    update doc strings

--- src/app/va/va_inst.rs
+++ src/app/va/va_inst.rs
@@ -152,4 +152,4 @@ impl msgf_inst::Inst for InstVa {
       in_number_frames: usize) {
         let sz = self.vcevec.len();
         let mut ch_ended = vec![false; sz];
-        self.vce_audio.
+        self.vce_audio.set_sample_number(in_number_fra

a33f6d6e04f8b11444ed8d8348b7467a9234baeb
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-08-03 17:17:29 +0000

    modify dockerfile

--- src/app/va/va_inst.rs
+++ src/app/va/va_inst.rs
@@ -158,4 +158,5 @@ impl msgf_inst::Inst for InstVa {
 
         //  All voices get together 
         for i in 0..sz {
-            if let Some(nt) = self.vcevec.
+            if let Some(nt) = self.vcevec.get_mut(i) {
+                ch_ended[i]

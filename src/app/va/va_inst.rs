f913f578753094237259b9cde2d276257d83359c
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-08-12 14:34:11 +0000

    fixed test

--- src/app/va/va_inst.rs
+++ src/app/va/va_inst.rs
@@ -166,4 +166,4 @@ impl msgf_inst::Inst for InstVa {
 
         //  make audio stereo
         abuf_l.mul_and_mix(&mut self.inst_audio, 1.0-self.pan);
-        abuf_r.m
+        abuf_r.mul_and_mix(&mut self.inst_audio, se

0b8a3ade840a845af3ffa78d59fa037062d9bde6
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-08-24 14:14:07 +0000

    remove semicolons

--- src/app/va/va_inst.rs
+++ src/app/va/va_inst.rs
@@ -191,4 +191,4 @@ impl InstVa {
         let prm = Rc::new(Cell::new(va_prm::TONE_PRM[inst_number]));
         Self {
             vce_audio: msgf_afrm::AudioFrame::new(0,msgf_if::MAX_BUFFER_SIZE),
-            inst_audio: msgf_afrm::Audi
+            inst_audio: msgf_afrm::AudioFrame::new(0,msgf_if::MAX_BUFFE

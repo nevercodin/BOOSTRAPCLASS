054485b7016b595e84c4265358a9d940812c34a6
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-08-24 08:55:20 +0000

    Update submodule ref

--- src/app/va/va_inst.rs
+++ src/app/va/va_inst.rs
@@ -190,4 +190,5 @@ impl InstVa {
         }
         let prm = Rc::new(Cell::new(va_prm::TONE_PRM[inst_number]));
         Self {
-            vce_audio: msgf_afrm::AudioFrame::new(0,msgf_if::MAX_BUFFER_SIZ
+            vce_audio: msgf_afrm::AudioFrame::new(0,msgf_if::MAX_BUFFER_SIZE),
+            inst_audio: msgf_afrm::Audi

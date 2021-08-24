49b5092d3e92de1227765989cbc8c0b6ec5705d3
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-08-24 22:15:52 +0000

    Adjusting README format

--- src/app/va/va_inst.rs
+++ src/app/va/va_inst.rs
@@ -193,4 +193,4 @@ impl InstVa {
             vce_audio: msgf_afrm::AudioFrame::new(0,msgf_if::MAX_BUFFER_SIZE),
             inst_audio: msgf_afrm::AudioFrame::new(0,msgf_if::MAX_BUFFER_SIZE),
             vcevec: Vec::new(),
-            delay
+            delay: msgf_delay::Delay::new(&prm.get().d

29a48a950546c43b67539dfda7dd5e53be81d501
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-09-03 21:29:32 +0000

    1.0.4 release

--- src/app/va/va_voice.rs
+++ src/app/va/va_voice.rs
@@ -45,3 +45,29 @@ impl PartialEq for VoiceVa {
     fn eq(&self, other: &Self) -> bool {
         self.note == other.note && self.vel == other.vel
     }
+}
+//---------------------------------------------------------
+impl msgf_voice::Voice for VoiceVa {
+    fn start_sound(&mut self) {
+        self.aeg.move_to_attack();
+        self.lfo.start();
+    }
+    fn note_off(&mut self) {
+        self.status = NoteStatus::AfterNoteOff;
+        self.aeg.move_to_release()
+    }
+    fn note_num(&self) -> u8 {self.note}
+    fn velocity(&self) -> u8 {self.vel}
+    fn change_pmd(&mut self, value: f32) {
+        self.osc.change_pmd(value);
+        self.emphasis_vol = 1.0 + value*6.0; // 1.0 - 1.5
+    }
+    fn amplitude(&mut self, volume: u8, expression: u8) {
+        self.max_note_vol = VoiceVa::calc_vol(volume, expression);
+    }
+    fn pitch(&mut self, pitch:f32) {
+        self.osc.change_pitch(pitch);
+    }
+    fn status(&self) -> NoteStatus {self.status}
+    fn damp(&mut self) {
+        self.status = NoteStatus::DuringDamp;

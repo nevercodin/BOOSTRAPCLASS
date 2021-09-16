cc9e6e998fc5fe1e06f079104ef1fa0fd8105193
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-09-16 13:59:55 +0000

    mm: mmu_notifier: make the mmu_notifier srcu static

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -5,4 +5,4 @@
 //  Created by Hasebe Masahiko on 2021/09/18.
 //  Copyright (c) 2021 Hasebe Masahiko.
 //  Released under the MIT license
-//  ht
+//  https://opensource.org/licenses/mit-license.php

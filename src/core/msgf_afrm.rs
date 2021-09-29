516d90ef00af2784ef2206b5897294892bbc0d25
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-09-29 05:47:32 +0000

    Updating dependencies...

--- src/core/msgf_afrm.rs
+++ src/core/msgf_afrm.rs
@@ -16,3 +16,42 @@ use crate::msgf_if;
 //---------------------------------------------------------
 pub struct AudioFrame {
     abuf: Vec<f32>,
+    pub sample_number: usize,
+    index: usize,
+}
+//---------------------------------------------------------
+//		Implements
+//---------------------------------------------------------
+impl AudioFrame {
+    pub fn new(mut sample_number: usize, total_size: usize) -> Self {
+        if sample_number > total_size {
+            sample_number = total_size;
+        }
+        Self {
+            abuf: vec![0.0; total_size],
+            sample_number,
+            index: 0,
+        }
+    }
+    pub fn set_sample_number(&mut self, snum: usize) {
+        self.sample_number = snum;      
+    }
+    pub fn copy_to_sysbuf(&self, ab: &mut [f32; msgf_if::MAX_BUFFER_SIZE]) {
+        for i in 0..self.sample_number {
+            ab[i] = self.abuf[i];
+        }
+    }
+    pub fn add_to_sysbuf(&self, ab: &mut [f32; msgf_if::MAX_BUFFER_SIZE]) {
+        for i in 0..self.sample_number {
+            ab[i] += self.abuf[i];
+        }
+    }
+    pub fn _copy_to_abuf(&self, ab: &mut AudioFrame) {
+        for i in 0..self.sample_number {
+            ab.abuf[i] = self.abuf[i];
+        }
+    }
+    pub fn put_into_abuf(&mut self, val: f32) {
+        self.abuf[self.index] = val;
+        self.index += 1;
+        if self.index >= self.sample_number {

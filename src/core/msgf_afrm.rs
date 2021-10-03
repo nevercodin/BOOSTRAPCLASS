ce84671a67fbc6603e0e8fecd99218d1a2d1d61e
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-10-03 14:19:18 +0000

    Fix Syntax Error

--- src/core/msgf_afrm.rs
+++ src/core/msgf_afrm.rs
@@ -55,3 +55,44 @@ impl AudioFrame {
         self.abuf[self.index] = val;
         self.index += 1;
         if self.index >= self.sample_number {
+            self.index = 0;
+        }
+    }
+    fn limit_check(val1: f32, val2: f32) -> f32 {
+        let mut newval = val1 + val2;
+        if newval > 1.0 {
+            newval = 0.99;
+        } else if newval < -1.0 {
+            newval = -0.99;
+        };
+        newval
+    }
+    pub fn clr_abuf(&mut self) {
+        for i in 0..self.sample_number {
+            self.abuf[i] = 0.0;
+        }
+    }
+    pub fn set_val(&mut self, num: usize, val: f32) {
+        let newval = Self::limit_check(val, 0.0);
+        self.abuf[num] = newval;
+    }
+    pub fn add_val(&mut self, num: usize, val: f32) {
+        let newval = Self::limit_check(self.abuf[num], val);
+        self.abuf[num] = newval;
+    }
+    pub fn mul_rate(&mut self, num: usize, rate: f32) {
+        let newval = Self::limit_check(self.abuf[num]*rate, 0.0);
+        self.abuf[num] = newval;
+    }
+    pub fn get_from_abuf(&self, num: usize) -> Option<f32> {
+        if num >= self.sample_number {
+            return None;
+        }
+        Some(self.abuf[num])
+    }
+    pub fn _get_max_level(&self) -> f32 {
+        let mut max_val: f32 = 0.0;
+        for i in 0..self.sample_number {
+            let val = self.abuf[i];
+            if max_val < val {
+                max_val = val;

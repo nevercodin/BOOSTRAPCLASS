e048b1665264c325253e24256dcf7db3d61abcb3
nevercodin <109537251+nevercodin@users.noreply.github.com>
2021-10-22 07:26:49 +0000

    Remove unused parameters

--- src/core/msgf_cfrm.rs
+++ src/core/msgf_cfrm.rs
@@ -6,4 +6,8 @@
 //  Created by Hasebe Masahiko on 2021/10/24.
 //  Copyright (c) 2021 Hasebe Masahiko.
 //  Released under the MIT license
-//  https://opensource.org/licenses/mit
+//  https://opensource.org/licenses/mit-license.php
+//
+use crate::msgf_if;
+
+//

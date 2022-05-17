72634a06301c8f4865cbbfe7b26a09e004ccd81a
nevercodin <109537251+nevercodin@users.noreply.github.com>
2022-05-17 05:37:59 +0000

    Update for language changes

--- src/core/msgf_disp.rs
+++ src/core/msgf_disp.rs
@@ -17,4 +17,4 @@ pub trait MsgfDisplay {
     fn print_prm<T: std::fmt::Display>(&self, string: &str, prm: T) {
         let mut all_str = String::from(string);
         let prm_str: &str = &prm.to_string();   //  to_string で、String型、&を付けると &str型
-   
+        all_str += prm_str;           

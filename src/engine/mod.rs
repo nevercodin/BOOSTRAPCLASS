4a8af03634bd2797706a362a3b236d783f200f4a
nevercodin <109537251+nevercodin@users.noreply.github.com>
2022-05-30 12:40:00 +0000

    Add Travis CI config file

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -2,4 +2,5 @@
 //  engine/mod.rs
 //	Musical Sound Generator Framework
 //
-//  Created by Hasebe Masahiko on 2
+//  Created by Hasebe Masahiko on 2021/10/15.
+//  Copyright (c) 2021 Hasebe Masahiko

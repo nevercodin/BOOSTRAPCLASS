c682d8756ebed33dfa298701b0c2741396fb3f0e
nevercodin <109537251+nevercodin@users.noreply.github.com>
2022-05-30 14:46:54 +0000

    annovar

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -3,4 +3,6 @@
 //	Musical Sound Generator Framework
 //
 //  Created by Hasebe Masahiko on 2021/10/15.
-//  Copyright (c) 2021 Hasebe Masahiko
+//  Copyright (c) 2021 Hasebe Masahiko.
+//  Released under the MIT license
+//

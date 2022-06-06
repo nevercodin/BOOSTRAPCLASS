4a84f3f6e0eb12a02bdc0ccfe250018bf18fa3e0
nevercodin <109537251+nevercodin@users.noreply.github.com>
2022-06-06 21:57:54 +0000

    [maven-release-plugin] prepare release v0.1

--- src/engine/msgf_aeg.rs
+++ src/engine/msgf_aeg.rs
@@ -15,3 +15,42 @@ use crate::engine::msgf_gen::*;
 //---------------------------------------------------------
 //		Synth. Parameter
 //---------------------------------------------------------
+#[derive(Copy, Clone)]
+pub struct AegParameter {
+    pub attack_rate: f32,
+    pub decay_rate: f32,
+    pub sustain_level: f32,
+    pub release_rate: f32,
+}
+//---------------------------------------------------------
+//		Definition
+//---------------------------------------------------------
+#[derive(PartialEq, Clone, Copy)]
+pub enum EgState {
+    NotYet,
+    Attack,     //  A
+    Decay,      //  D
+    Sustain,    //  S
+    Release,    //  R
+    EgDone,
+    _Damp,
+}
+pub struct Aeg {
+    prms: AegParameter,
+    state: EgState,
+    tgt_value: f32,
+    src_value: f32,
+    crnt_value: f32,
+    crnt_rate: f32,
+    interpolate_value: f32,
+    release_rsv: bool,
+    last_value: f32,
+}
+//---------------------------------------------------------
+//		Implements
+//---------------------------------------------------------
+impl Aeg {
+    pub fn new(ref_prms: &AegParameter) -> Aeg {
+        Aeg {
+            prms: *ref_prms,
+            state: EgState::NotYet,

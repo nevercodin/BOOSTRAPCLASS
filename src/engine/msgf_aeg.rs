3db168b7c39adc0d31c25dd6e500e43bf125a7fe
nevercodin <109537251+nevercodin@users.noreply.github.com>
2022-06-07 17:31:21 +0000

    now working

--- src/engine/msgf_aeg.rs
+++ src/engine/msgf_aeg.rs
@@ -54,3 +54,42 @@ impl Aeg {
         Aeg {
             prms: *ref_prms,
             state: EgState::NotYet,
+            tgt_value: 0.0,
+            src_value: 0.0,
+            crnt_value: 0.0,
+            crnt_rate: 1.0,
+            interpolate_value: 0.0,
+            release_rsv: false,
+            last_value: 0.0,
+        }
+    }
+    pub fn move_to_attack(&mut self) {
+        self.src_value = 0.0;
+        self.tgt_value = 1.0;
+        self.crnt_rate = self.prms.attack_rate;
+        self.state = EgState::Attack;
+        self.interpolate_value = 0.0;
+        self.release_rsv = false;
+    }
+    fn move_to_decay(&mut self, eg_crnt: f32) {
+        if self.prms.decay_rate == 1.0 {
+            self.move_to_sustain(eg_crnt);
+        } else {
+            self.src_value = eg_crnt;
+            self.tgt_value = self.prms.sustain_level;
+            self.crnt_rate = self.prms.decay_rate;
+            self.state = EgState::Decay;
+            self.interpolate_value = 0.0;
+        }
+    }
+    fn move_to_sustain(&mut self, eg_crnt: f32) {
+        if self.prms.sustain_level == 0.0 {
+            self.move_to_egdone();
+        } else {
+            self.src_value = eg_crnt;
+            self.tgt_value = self.prms.sustain_level;
+            self.crnt_rate = 0.0;
+            self.state = EgState::Sustain;
+            self.interpolate_value = 0.0;
+        }
+    }

e76dfce8b171b24425a62c32ad08507767fa16db
nevercodin <109537251+nevercodin@users.noreply.github.com>
2022-06-30 20:44:38 +0000

    Material Search

--- src/engine/msgf_delay.rs
+++ src/engine/msgf_delay.rs
@@ -57,4 +57,5 @@ impl Delay {
     }
 }
 impl Engine for Delay {
-    fn process_as(&mut self, in_abuf: [&mut msgf_a
+    fn process_as(&mut self, in_abuf: [&mut msgf_afrm::AudioFrame;2]) {
+        let 

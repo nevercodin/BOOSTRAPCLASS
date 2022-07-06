6718840040890b5f7aec8784e1f0217ed0ca132b
nevercodin <109537251+nevercodin@users.noreply.github.com>
2022-07-06 13:10:13 +0000

    Changed directory structure

--- src/engine/msgf_delay.rs
+++ src/engine/msgf_delay.rs
@@ -64,4 +64,5 @@ impl Engine for Delay {
                 if let Some(input_dt) = in_abuf[str].get_from_abuf(i)  {
                     let mut crnt_dt: f32 = 0.0;
                     if let Some(output_dt) = self.delay_buffer[str].get_from_abuf(self.rd_ptr[str]) {
-                        crnt_dt = input_dt + output_dt*self.att_
+                        crnt_dt = input_dt + output_dt*self.att_ratio;
+                        if crn

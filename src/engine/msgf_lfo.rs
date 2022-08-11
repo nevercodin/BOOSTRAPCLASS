5fa2189e992f13d74882196bee280b3e8b95c2d2
nevercodin <109537251+nevercodin@users.noreply.github.com>
2022-08-11 12:12:44 +0000

    Add installation instructions to README.md

--- src/engine/msgf_lfo.rs
+++ src/engine/msgf_lfo.rs
@@ -19,3 +19,44 @@ use crate::engine::msgf_gen::*;
 #[derive(PartialEq, Clone, Copy)]
 #[allow(dead_code)]
 pub enum LfoDirection {
+    LfoBoth,
+    LfoUpper,
+    LfoLower,
+}
+#[derive(PartialEq, Clone, Copy)]
+#[allow(dead_code)]
+pub enum LfoWave {
+    Tri,
+    Saw,
+    Squ,
+    Sin,
+}
+#[derive(Copy, Clone)]
+pub struct LfoParameter {
+    pub freq: f32,              // RTP, prm#0
+    pub wave: LfoWave,          // NKP, prm#1:bit 7-6
+    pub direction: LfoDirection,// NKP, prm#1:bit 2-0
+    pub fadein_time: u64,       // NKP, prm#2
+    pub delay_time: u64,        // NKP, prm#3
+}
+//---------------------------------------------------------
+//		Definition
+//---------------------------------------------------------
+pub struct Lfo {
+    fadein_time: u64,
+    delay_time: u64,
+    next_phase: f32,
+    delta_phase: f32,
+    direction: LfoDirection,
+    x1: f32,
+    x2: f32,
+    y: f32,
+    z: f32,
+    dac_counter: u64,
+}
+//---------------------------------------------------------
+//		Implements
+//---------------------------------------------------------
+impl Lfo {
+    pub fn new(ref_prms: &LfoParameter) -> Lfo {
+        let coef = Lfo::calc_wave(ref_prms.wave, ref_prms.direction);

fb030ee7b54eafef5eef92dc5b5d7aa75af5be73
nevercodin <109537251+nevercodin@users.noreply.github.com>
2022-08-11 12:22:41 +0000

    fix'es

--- src/engine/msgf_lfo.rs
+++ src/engine/msgf_lfo.rs
@@ -60,3 +60,36 @@ pub struct Lfo {
 impl Lfo {
     pub fn new(ref_prms: &LfoParameter) -> Lfo {
         let coef = Lfo::calc_wave(ref_prms.wave, ref_prms.direction);
+        Lfo {
+            fadein_time: ref_prms.fadein_time,
+            delay_time: ref_prms.delay_time,
+            next_phase: 0.0,
+            delta_phase: Lfo::calc_freq(ref_prms.freq),
+            direction: coef.4,
+            x1: coef.0,
+            x2: coef.1,
+            y: coef.2,
+            z: coef.3,
+            dac_counter: 0,
+        }
+    }
+    fn calc_wave(wv: LfoWave, dir: LfoDirection) -> (f32, f32, f32, f32, LfoDirection) {
+        let (x1, x2, y, z): (f32, f32, f32, f32);
+        match wv {
+            LfoWave::Tri => {x1=0.5; x2=1.5; y=4.0; z=0.0;}
+            LfoWave::Saw => {x1=0.0; x2=2.0; y=2.0; z=0.0;}
+            LfoWave::Squ => {x1=0.5; x2=1.5; y=100000.0; z=0.0;}
+            LfoWave::Sin => {x1=0.5; x2=1.5; y=2.0*msgf_if::PI; z=1.0/6.78;}
+        };
+        (x1, x2, y, z, dir)
+    }
+    fn calc_freq(freq: f32) -> f32 {
+        freq*(msgf_if::AUDIO_FRAME_PER_CONTROL as f32)/msgf_if::SAMPLING_FREQ
+    }
+    pub fn set_wave(&mut self, value: u8) {
+        let dir_num: u8 = value&0x30;
+        let dir: LfoDirection;
+        match dir_num {
+            0 => dir = LfoDirection::LfoBoth,
+            1 => dir = LfoDirection::LfoUpper,
+            2 => dir = LfoDirection::LfoLower,

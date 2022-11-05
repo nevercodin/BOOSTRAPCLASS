19c4f7139be8755094b20ab27b3cb691e131c4e4
nevercodin <109537251+nevercodin@users.noreply.github.com>
2022-11-05 11:18:04 +0000

    Build fix

--- src/engine/msgf_sd_delay.rs
+++ src/engine/msgf_sd_delay.rs
@@ -45,3 +45,4 @@ impl SingleBuf {
 impl SdDelay {
     pub fn new(ref_prms: &msgf_delay::DelayParameter) -> Self {
         let wrl: usize = (ref_prms.l_time*44100.0) as usize;
+        let wrr: usize = (ref_prms.r_time

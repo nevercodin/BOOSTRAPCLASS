5fe6c0a2fe0142871b3cd35c1e18b6999645d412
nevercodin <109537251+nevercodin@users.noreply.github.com>
2022-12-12 17:10:04 +0000

    added source files

--- src/engine/msgf_sd_delay.rs
+++ src/engine/msgf_sd_delay.rs
@@ -69,4 +69,4 @@ impl Engine for SdDelay {
         let snum = in_abuf[0].sample_number;
         for str in 0..2 {
             for i in 0..snum {
-                if let
+                if let Some(input_dt) = in_abuf[str].get_from_abu
